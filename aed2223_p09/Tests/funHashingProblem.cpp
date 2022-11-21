afc2678799018e4e436fb71b200e8a042812d0ef
DiogoSant0s <up202009291@up.pt>
2022-11-21 14:52:04 +0000

    Exercise 9

---
+++ aed2223_p09/Tests/funHashingProblem.cpp
@@ -0,0 +1,12 @@
+#include "funHashingProblem.h"
+#include <unordered_set>
+
+FunHashingProblem::FunHashingProblem() {}
+
+// -------------------------------------
+
+// TODO
+vector<int> FunHashingProblem::findDuplicates(const vector<int>& values, int k) {
+    vector<int> res;
+    return res;
+}
