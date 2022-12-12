6c703d137e85a439697c1b5286bfa93aa406b6a1
DiogoSant0s <up202009291@up.pt>
2022-12-12 14:58:52 +0000

    Exercises 11 and 12

---
+++ aed2223_p12/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/up202009291/Desktop/GitHub/AED2022-23/aed2223_p12/lib/googletest-master
+# Build directory: C:/Users/up202009291/Desktop/GitHub/AED2022-23/aed2223_p12/cmake-build-debug/lib/googletest-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
