00179c5b67f691e2f2808b647b6948bb26a94501
DiogoSant0s <113988599+DiogoSant0s@users.noreply.github.com>
2023-01-04 19:46:55 +0000

    Delete aed2223_p06/cmake-build-debug directory

--- aed2223_p06/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/up202009291/Desktop/aed2223_p06/lib/googletest-master/googlemock
-# Build directory: C:/Users/up202009291/Desktop/aed2223_p06/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
